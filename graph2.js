39f1c3fa4d75e6163290ea10f5f7d7ca3c4591d3
Aemiyh <kotorisuzume@gmail.com>
2020-01-09 10:41:12 +0100

    test graph

--- graph2.js
+++ graph2.js
@@ -1,7 +1,7 @@
 var data = [
   { id: "TVs", value: 11, image: "img/television.svg", type: "production" },
-  { id: "Ordinateurs", value: 17, image: "img/computer.svg",type: "production" },
-  { id: "Terminaux", value: 20, image: "img/responsive.svg" ,type: "utilisation"},
+  { id: "Ordinateurs", value: 17, image: "img/computer.svg " ,type: "production"},
+  { id: "Terminaux", value: 20, image: "img/responsive.svg",type: "utilisation" },
   { id: "Smartphones", value: 11, image: "img/smartphone.svg" ,type: "production"},
   { id: "Autres", value: 6, image: "img/more.svg",type: "production" },
   { id: "Réseaux", value: 16, image: "img/cloud.svg" ,type: "utilisation"},
