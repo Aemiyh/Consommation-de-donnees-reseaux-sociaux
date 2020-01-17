a4d5d4fb3d037c8b05dfdad902dc357ce0d981ee
Aemiyh <kotorisuzume@gmail.com>
2020-01-17 13:04:07 +0100

    add img

--- graph2.js
+++ graph2.js
@@ -1,7 +1,7 @@
 var data = [
   { id: "TVs", value: 11, image: "img/television.png", type: "production" },
   { id: "Ordinateurs", value: 17, image: "img/computer.svg " ,type: "production"},
-  { id: "Terminaux", value: 20, image: "img/responsive.svg",type: "utilisation" },
+  { id: "Terminaux", value: 20, image: "img/online-shop.svg",type: "utilisation" },
   { id: "Smartphones", value: 11, image: "img/iphone.png" ,type: "production"},
   { id: "Autres", value: 6, image: "img/more.svg",type: "production" },
   { id: "Réseaux", value: 16, image: "img/cloud.svg" ,type: "utilisation"},
@@ -15,7 +15,8 @@ new d3plus.Treemap()
   .shapeConfig({
     backgroundImage: function (d) {
       return d.image;
-    }
+    },
+    label:false
   })
   .tooltipConfig({
     body: function (d) {
